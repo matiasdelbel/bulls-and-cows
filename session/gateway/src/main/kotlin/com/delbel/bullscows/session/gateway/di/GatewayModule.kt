9974426a948cf51ed3af63a15d2989e9dfdee05e
Matias Del Bel <matiasdelbel@gmail.com>
2020-04-13 16:47:19 -0300

    current session

--- session/gateway/src/main/kotlin/com/delbel/bullscows/session/gateway/di/GatewayModule.kt
+++ session/gateway/src/main/kotlin/com/delbel/bullscows/session/gateway/di/GatewayModule.kt
@@ -1,13 +1,18 @@
 package com.delbel.bullscows.session.gateway.di
 
+import com.delbel.bullscows.session.domain.CurrentSession
 import com.delbel.bullscows.session.domain.repository.SessionRepository
+import com.delbel.bullscows.session.gateway.LocalCurrentSession
 import com.delbel.bullscows.session.gateway.LocalSessionRepository
 import dagger.Binds
 import dagger.Module
 
-@Module(includes = [DatabaseModule::class])
+@Module(includes = [DatabaseModule::class, PreferencesModule::class])
 abstract class GatewayModule {
 
     @Binds
     internal abstract fun bindRepository(repository: LocalSessionRepository): SessionRepository
+
+    @Binds
+    internal abstract fun bindCurrentSession(session: LocalCurrentSession): CurrentSession
 }
